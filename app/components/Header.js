5badcdbc6b71b1d9e169d4530a57433eddff2630
nesrin <103462967+Mnasrinesrine@users.noreply.github.com>
2025-10-26 17:34:06 +0100

    nesrine
    some changes

--- app/components/Header.js
+++ app/components/Header.js
@@ -7,14 +7,14 @@ import { motion } from "framer-motion";
 
 const Header = () => {
   return (
-    <header className="bg-white relative bg-[url('/images/header2.jpeg')] bg-cover bg-center text-white rounded-b-3xl pb-20 lg:h-[600px] sm:h-[400px] ">
-      <div className="absolute inset-0 bg-black/50 rounded-b-3xl"></div>
+    <header className="bg-white relative bg-[url('/images/header2.jpeg')] bg-cover bg-center text-white rounded-b-4xl pb-20 lg:h-[600px] sm:h-[550px] ">
+      <div className="absolute inset-0 bg-black/70 rounded-b-4xl"></div>
 
       <motion.div
         initial={{ opacity: 0, y: 40 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
-        className="relative z-10 max-w-2xl px-4 mx-auto py-8 flex flex-col items-center text-center gap-4"
+        className="relative z-10 flex flex-col items-center  text-center gap-4 pt-32  px-2 lg:px-0 max-w-3xl mx-auto"
       >
         <p className="text-sm tracking-wide uppercase ">
           Hello, I'm Nesrine Nasri
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -29,23 +29,39 @@ const Navbar = () => {
 
       {/* Desktop Menu */}
       <div className="hidden lg:flex gap-6 items-center">
-        <Link href="#about" className="hover:text-cyan-400 transition">
+        <Link
+          href="#about"
+          className="transition hover:underline hover:decoration-2 hover:underline-offset-4"
+        >
           About
         </Link>
-        <Link href="#projects" className="hover:text-cyan-400 transition">
+        <Link
+          href="#projects"
+          className="transition hover:underline hover:decoration-2 hover:underline-offset-4"
+        >
           Projects
         </Link>
-        <Link href="#services" className="hover:text-cyan-400 transition">
+        <Link
+          href="#services"
+          className="transition hover:underline hover:decoration-2 hover:underline-offset-4"
+        >
           Services
         </Link>
-        <Link href="#testimonials" className="hover:text-cyan-400 transition">
+        <Link
+          href="#testimonials"
+          className="transition hover:underline hover:decoration-2 hover:underline-offset-4"
+        >
           Testimonials
         </Link>
       </div>
 
       {/* Mobile Menu */}
       <div className="lg:hidden">
-        <button onClick={() => setMenuOpen(!menuOpen)}>
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle Menu"
+          className="text-white cursor-pointer focus:outline-none"
+        >
           <FiAlignRight size={28} />
         </button>
       </div>
@@ -56,10 +72,30 @@ const Navbar = () => {
           animate={{ opacity: 1, y: 0 }}
           className="absolute top-18 right-0 left-0 bg-Primary text-white p-4 rounded-lg shadow-lg z-50 flex flex-col gap-2"
         >
-          <Link href="#about">About</Link>
-          <Link href="#projects">Projects</Link>
-          <Link href="#services">Services</Link>
-          <Link href="#testimonials">Testimonials</Link>
+          <Link
+            href="#about"
+            className="transition hover:underline hover:decoration-2 hover:underline-offset-4 "
+          >
+            About
+          </Link>
+          <Link
+            href="#projects"
+            className="transition hover:underline hover:underline-offset-4 "
+          >
+            Projects
+          </Link>
+          <Link
+            href="#services"
+            className="transition hover:underline hover:underline-offset-4 "
+          >
+            Services
+          </Link>
+          <Link
+            href="#testimonials"
+            className="transition hover:underline hover:underline-offset-4 "
+          >
+            Testimonials
+          </Link>
         </motion.div>
       )}
     </motion.nav>
